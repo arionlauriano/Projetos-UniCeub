e695e84f9c1233444daf06a782b0396b4709877b
Camila R Lauriano <camila@lauriano.org>
2025-08-25 17:30:56 -0300

    Update autores_dao.py

--- Prog_Orient_Obj/PrjLivros/Aula02/DAO/autores_dao.py
+++ Prog_Orient_Obj/PrjLivros/Aula02/DAO/autores_dao.py
@@ -1,26 +1,58 @@
-import mysql.python
+import mysql.connector
 
 # Definição da classe:
 class autores:
     # Inicializa os Atributos
         # Ele pega e define qual atributo chamar?
-    def __init__(self, cod_autor= None, nome_autor=""):
+    def __init__(self, nome_autor="", cod_autor= None):
         self.cod_autor = cod_autor
         self.nome_autor = nome_autor
     def __str__(self):
         return f"Id: {self.cod_autor} Nome: {self.nome_autor}"
     
-# Definição da conexão com a database
+# Definição da classe que cuidará das operações dentro do manco de dados
 class autoresDAO:
-    def __init__(self, host="localhost", user="root", password="atuasenha", database="colecao_livros"):
+# Faz a conexão 
+    def __init__(self, host="localhost", user="root", password="Senha#", database="colecao_livros"):
+        try:
+            self.conexao = mysql.connector.connect(
+                host=host,
+                user=user,
+                password=password,
+                database=database
+            )
+            self.cursor = self.conexao.cursor()
+            print("Conexão com o banco de dados estabelecida.")
+        except mysql.connector.Error as err:
+            print(f"Erro ao conectar com a database: {err}")
+            self.conexao = None
+            self.cursor = None
+# Método para inserir um novo objeto
+    def inserir(self, autores):
+        # Ele não vai nem tentar se não houver conexão
+        if not self.conexao:
+            print("Erro: não há conexão com a database.")
+            return # Encerra o processamento da função "inserir()"
     
-    self.conexao = mysql.connector.connect(
-        host=host,
-        user=user,
-        password=password,
-        database=database
-        )
-    self.cursor = self.conexao.cursor()
-    print("Conexão com o banco de dados estabelecida.")
-    self.conexao = None
-    self.cursor = None
+    sql = "INSERT INTO autores (nome_autores) VALUES (%s)"
+    valores = (autores.nome_autor)
+    # Try faz teste e caso não haja erro o except é ignorado
+    try:
+        self.cursor.execute(sql, valores)
+        self.cursor.commit()
+        print(f"Autor '{autor.nome_autor}' foi inserido.")
+        autor.cod_autor = self.curoso.lastrowid
+    except mysql.connecotr.Error as err:
+        print(f"Erro ao inserir montadora; {err}")
+        self.conexao.rollback()
+
+
+if __name__ == "__main__":
+    #roda a função autoresDAO() estabelecendo conexão
+    dao = autoresDAO()
+
+    # Caso a conexão exista, cria objetos Autores
+    autor1= autores("J.K. Rouling")
+
+    # Usa a função inserir()
+    dao.inserir(autor1)
