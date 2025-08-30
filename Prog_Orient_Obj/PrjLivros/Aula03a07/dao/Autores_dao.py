529c9df1154406f82a7cad2ca84e1d856776b64b
Camila R Lauriano <camila@lauriano.org>
2025-08-29 22:17:35 -0300

    Update Autores_dao.py

--- Prog_Orient_Obj/PrjLivros/Aula03a07/dao/Autores_dao.py
+++ Prog_Orient_Obj/PrjLivros/Aula03a07/dao/Autores_dao.py
@@ -40,20 +40,97 @@ class AutoresDAO:
 
     def select_alfabetico(self):
         if not self.conexao:
-            return
+            print("10conectado")
+            return []
         
         sql = "SELECT * FROM autores ORDER BY nome_autor"
 
         try:
-           self.cursor.execute(sql)
-           resultados = self.cursor.fetchall()
+            self.cursor.execute(sql)
+            resultados = self.cursor.fetchall()
+            lista_autores = []
+            for row in resultados:
+               autores = Autores(cod_autor=row[0], nome_autor=row[1])
+               lista_autores.append(autores)
+            return lista_autores
+        except mysql.connector.Error as err:
+            print(f"Erro ao selecionar autores: {err}")
+            return []
+        
+    def consulta_por_id(self,cod):
+        if not self.conexao:
+            return None
+        sql = "SELECT * FROM autores WHERE cod_autor=%s"
 
-           lista_autores = []
+        try:
+            self.cursor.execute(sql, [cod])
+            resultado = self.cursor.fetchone()
+            if resultado is None:
+                return None
+            else:
+                autor=Autores[cod_autor=resultado[0], nome_autor=resultado[1]]
+                return autor
+        except mysql.connector.Error as err:
+            return None
+        
+    def atualizar(self, autor):
+        if not self.conexao:
+            return
+        
+        sql = "UPDATE autores SET nome_autor=%s WHERE cod_autor=%s"
+        valores = (autor.nome_autor, autor.cod_autor)
 
+        try:
+            self.cursor.execute(sql, valores)
+            self.conexao.commit()
+            print(f"Autor de código {autor.cod_autor} atualizado.")
+        except mysql.connector.Error as err:
+            print(f"Erro ao atualizar autor de código {Autores.cod_autor}: {err}")
+            self.conexao.rollback()
 
+    def deletar(self, cod_autor):
+        if not self.conexao:
+            return
+        
+        sql = "DELETE FROM autores WHERE cod_autor=%s"
+
+        try:
+            self.cursor.execute(sql, [cod_autor])
+            self.conexao.commit()
+            print(f"Autor de código {cod_autor} deletado.")
+        except mysql.connector.Error as err:
+            print(f"Erro ao deletar autor de código {cod_autor}: {err}")
+            self.conexao.rollback()
 
 
 if __name__ == "__main__":
     dao = AutoresDAO()
+
+    if dao.conexao:
+        autor1=Autores("Philip Pullman")
+        autor2=Autores("Agatha Christie")
+
+        dao.inserir_autores(autor1)
+        dao.inserir_autores(autor2)
+
+        print("\nAutores Cadastrados:")
+        for autor in dao.select_alfabetico():
+            print(autor)
+
+        if autor1.cod_autor:
+            print(f"\nAtualizando autor de código {autor1.cod_autor}...")
+            autor1.nome_autor="Jorge"
+            dao.atualizar(autor1)
+
+        if autor2.cod_autor:
+            print(f"\nDeletando autor de código {autor2.cod_autor}...")
+            dao.deletar(autor2.cod_autor)
+
+        print("\nLista Atualizada:")
+        for autor in dao.select_alfabetico():
+            print(autor)
+
+
+
 
         
